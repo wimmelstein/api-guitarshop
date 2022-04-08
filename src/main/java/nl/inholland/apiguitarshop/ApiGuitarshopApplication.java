f92fad05e5c8f73d37207f2bb75ba1235db67286
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2022-04-08 11:07:04 +0200

    Added context and gave the application a name

--- src/main/java/nl/inholland/apiguitarshop/ApiGuitarshopApplication.java
+++ src/main/java/nl/inholland/apiguitarshop/ApiGuitarshopApplication.java
@@ -2,12 +2,15 @@ package nl.inholland.apiguitarshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ApiGuitarshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGuitarshopApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(ApiGuitarshopApplication.class, args);
+		Arrays.stream(ctx.getBeanDefinitionNames()).forEach(System.out::println);
 	}
-
 }
