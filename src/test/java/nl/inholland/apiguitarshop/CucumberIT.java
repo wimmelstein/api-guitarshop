e51b9c298e1a1e393b9ec44ebe533388e7b2a37d
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2022-05-22 12:58:58 +0200

    Moved features to its own directory

--- src/test/java/nl/inholland/apiguitarshop/CucumberIT.java
+++ src/test/java/nl/inholland/apiguitarshop/CucumberIT.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources",
+        features = "src/test/resources/features",
         glue = "nl.inholland.apiguitarshop.steps",
         plugin = "pretty",
         publish = true)
