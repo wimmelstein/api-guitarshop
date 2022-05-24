1853529e1869d65d2c9ae41a45cd58d368675f8a
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2022-05-24 19:23:58 +0200

    Fixed comment

--- src/test/java/nl/inholland/apiguitarshop/steps/guitars/GuitarStepdefs.java
+++ src/test/java/nl/inholland/apiguitarshop/steps/guitars/GuitarStepdefs.java
@@ -16,7 +16,7 @@ import org.springframework.http.ResponseEntity;
 
 public class GuitarStepdefs extends BaseStepDefinitions implements En {
 
-    // Token is valid for one year , user has Role.USER
+    // Token is valid for one year
     private static final String VALID_TOKEN_USER = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyIiwiYXV0aCI6W10sImlhdCI6MTY1MzMxMTc0NiwiZXhwIjoxNjg0ODQ3NzQ2fQ.itSjs-evCYi2P7JAKwT4DY8u5RIASTghoaeQOa33v_s";
     private static final String VALID_TOKEN_ADMIN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOltdLCJpYXQiOjE2NTMzMTE4MjQsImV4cCI6MTY4NDg0NzgyNH0.heZJFGgEEdaUvEhbjnbK7PFfC_BfxOMIvmRq8fjvwMs";
     private static final String EXPIRED_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyIiwiYXV0aCI6W10sImlhdCI6MTY1MzMxMTkwNSwiZXhwIjoxNjUzMzExOTA1fQ.mKFrXM15WCXVNbSFNpqYix_xsMjsH_M31hiFf-o7JXs";
