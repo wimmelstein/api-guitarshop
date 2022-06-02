6e4bd8d915d6818369367e70ef7a4e1b6828b52b
Wim Wiltenburg <wim.wiltenburg@mendesgans.nl>
2022-06-02 17:54:00 +0200

    Get request is now 200

--- src/test/java/nl/inholland/apiguitarshop/controller/GuitarControllerTest.java
+++ src/test/java/nl/inholland/apiguitarshop/controller/GuitarControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
@@ -33,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(GuitarController.class)
+@Import(GuitarController.class)
 @ContextConfiguration(classes = TestConfig.class)
 class GuitarControllerTest {
 
