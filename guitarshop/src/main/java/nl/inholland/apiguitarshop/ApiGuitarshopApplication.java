176ca449a572aed0cdb526488913bffa84db3319
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2023-03-05 11:45:06 +0100

    Moved some things around

--- guitarshop/src/main/java/nl/inholland/apiguitarshop/ApiGuitarshopApplication.java
+++ guitarshop/src/main/java/nl/inholland/apiguitarshop/ApiGuitarshopApplication.java
@@ -1,5 +1,7 @@
 package nl.inholland.apiguitarshop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -7,9 +9,11 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class ApiGuitarshopApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ApiGuitarshopApplication.class.getName());
+
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(ApiGuitarshopApplication.class, args);
-
+		LOG.info("Application started");
 	}
 
 
